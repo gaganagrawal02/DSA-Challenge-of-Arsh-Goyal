3778f334f9d9d45f2ed5ac972b839d0cf8f28438
Gagan Agrawal <134318471+gaganagrawal02@users.noreply.github.com>
2024-07-22 23:30:41 +0530

    7 day

---
+++ src/JumpGame.java
@@ -0,0 +1,16 @@
+public class JumpGame {
+    public static boolean canJump(int[] nums) {
+        int lastPos = nums.length - 1;
+        for (int i = nums.length - 2; i >= 0; i--) {
+            if (i + nums[i] >= lastPos) {
+                lastPos = i;
+            }
+        }
+        return lastPos == 0;
+    }
+    public static void main(String[] args) {
+        int[] nums1 = {2, 3, 1, 1, 4};
+        int[] nums2 = {3, 2, 1, 0, 4};
+        System.out.println(canJump(nums1));
+    }
+}
